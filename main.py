42d3d28d7b9bb8f479dac35b2ec06bf4b10cdd7b
Naoki Kinoshita <uttnaoki@gmail.com>
2022-03-27 13:18:16 +0900

    材料詳細を取得

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from scrape_links import ScrapeLinks
 
+domain_url = 'https://astroneer.fandom.com'
 url = 'https://astroneer.fandom.com/ja/wiki/カテゴリ:大型'
 
 links = ScrapeLinks()
+links.domain_url = domain_url
 links.url = url
-print(links.scrape_links())
+links.scrape_details()
--- scrape_links.py
+++ scrape_links.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+import re
+
+DELAY_TIME = 3
 
 class ScrapeLinks():
     def __init__(self):
+        self.domain_url = ''
         self.url = ''
 
     def shape_a_tag(self, a_tag):
@@ -17,3 +22,19 @@ class ScrapeLinks():
         a_tag_list = soup.select('div.mw-category-group a')
         return [ self.shape_a_tag(a_tag) for a_tag in a_tag_list ]
 
+    def scrape_details(self):
+        links = self.scrape_links()
+        # for link in [links[3]]:
+        for link in links:
+            response = requests.get(self.domain_url + link['url'])
+            soup = BeautifulSoup(response.text, 'html.parser')
+            tr_list = soup.select('table.infoboxtable tr')
+            time.sleep(DELAY_TIME)
+            if len(tr_list) < 8:
+                continue
+            td = tr_list[7].select('td')[0]
+            print(td.text)
+            
+            num_list = re.sub(r'\D+', ' ', td.text).split()
+            mat_list = re.sub(r'[\dx ]+', ' ', td.text).split()
+            print([ { 'name': mat_list[i], 'num': num_list[i] } for i in range(len(num_list))])
